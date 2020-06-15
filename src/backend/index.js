9bfbc35a4b97e188acaffb7af0fd65e016d878bd
mervenuslu <merven.uslu@gmail.com>
2020-06-15 23:55:39 +0300

    fix port

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 const mongoose = require('mongoose')
 const api = require('./routes/routes');
 const bodyParser = require('body-parser');
-
+console.log("Server has starting")
 mongoose
   .connect("mongodb://merve:asd1merve@ds143893.mlab.com:43893/heroku_pg6qhvf5", { useNewUrlParser: true })
   .then(() => {
@@ -11,7 +11,7 @@ mongoose
     app.use(bodyParser.json());
     app.use("/api/v1", api);
    
-    var port = process.env.PORT || 8080;
+    const port = process.env.PORT || 4000;
     app.listen(port, () => {
       console.log("Server has started!")
     })
